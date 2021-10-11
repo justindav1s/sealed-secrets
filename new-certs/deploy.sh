7967f9188181bb556ca66fe098022881f7d43a8b
Justin Davis <justinndavis@gmail.com>
2021-10-11 16:07:40 +0100

    new-cert update

--- new-certs/deploy.sh
+++ new-certs/deploy.sh
@@ -8,15 +8,15 @@ rm -rf ${DOMAIN}*
 
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:4096 -subj '/O=Sealed Secrets/CN=example.com' -keyout example.com.key.pem -out example.com.crt.pem
 
-oc create secret tls sealed-secret-${DATE} \
+oc create secret tls sealed-secret-cert-${DATE} \
      --cert=example.com.crt.pem \
      --key=example.com.key.pem \
      --dry-run=client \
      -o yaml \
      -n sealed-secrets \
-     > sealed-secret-${DATE}.yaml
+     > sealed-secret-cert-${DATE}.yaml
 
 
-oc apply -f sealed-secret-${DATE}.yaml
+oc apply -f sealed-secret-cert-${DATE}.yaml
 
-oc label secret sealed-secret-${DATE} sealedsecrets.bitnami.com/sealed-secrets-key=active -n sealed-secrets
+oc label secret sealed-secret-cert-${DATE} sealedsecrets.bitnami.com/sealed-secrets-key=active -n sealed-secrets
